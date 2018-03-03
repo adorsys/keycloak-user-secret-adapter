2a28fceda51a002b4c0beb3f000e5d19dfde70cb
Denys Golubiev <dgo@adorsys.de>
2018-03-03 16:58:36 +0100

    Change request type in test script to GET

--- keycloak-user-secret-embedded-plugin/src/scripts/client/1-mock-ba.bash
+++ keycloak-user-secret-embedded-plugin/src/scripts/client/1-mock-ba.bash
@@ -12,4 +12,4 @@ TKN=$2
 
 HEADER_DUMP_FILE="$0.txt"
 
-curl -s -X POST "http://$hostAndPort/bankaccesses" -D ${HEADER_DUMP_FILE} -H "Authorization: Bearer $TKN"
+curl -s -X GET "http://$hostAndPort/bankaccesses" -D ${HEADER_DUMP_FILE} -H "Authorization: Bearer $TKN"
